5bb6e4867d1f8753b2570dc954d394a9723fe488
Rabeka <alex.rebekah@gmail.com>
2026-07-27 18:24:40 +0530

    Add Internet Gateway module and integrate networking foundation

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -4,3 +4,18 @@ module "vpc" {
   vpc_name = "engineering-simulation-dev"
   vpc_cidr = "10.0.0.0/16"
 }
+
+module "subnets" {
+  source = "../../modules/subnets"
+
+  vpc_id               = module.vpc.vpc_id
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+}
+
+module "internet_gateway" {
+  source = "../../modules/internet-gateway"
+
+  vpc_id = module.vpc.vpc_id
+}
--- infrastructure/terraform/environments/dev/variables.tf
+++ infrastructure/terraform/environments/dev/variables.tf
@@ -2,3 +2,21 @@ variable "aws_region" {
   type    = string
   default = "ap-south-1"
 }
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
