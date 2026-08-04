2bc3440483f120e3f3d1fd6ec3bd421b1711d5b3
Rabeka <alex.rebekah@gmail.com>
2026-08-04 22:52:19 +0530

    fix(ci): correct terraform formatting

--- environments/state-lock-lab/main.tf
+++ environments/state-lock-lab/main.tf
@@ -6,7 +6,7 @@ resource "terraform_data" "example" {
   input = "state-lock-lab-updated"
 }
 
-resource   "terraform_data"   "format_failure"{
-input="ci-format-test"
+resource "terraform_data" "format_failure" {
+  input = "ci-format-test"
 }
 
